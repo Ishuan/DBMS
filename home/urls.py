be0c9073bf982377c363a19130d5b1b289ec5264
Ishan <ishanagarwal.cs@gmail.com>
2017-04-15 05:35:17 -0400

    Update 3:Have resolved the dropdown issue

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.start, name="start"),
     url(r'^home', views.home, name='home'),
-    url(r'^BIOINFO', views.bio, name='bio'),
+    url(r'^bio', views.bio, name='bio'),
     url(r'^CS', views.cs, name='cs'),
     url(r'^SIS', views.sis, name='sis'),
+    url(r'^fac', views.fac, name='sis'),
 ]
